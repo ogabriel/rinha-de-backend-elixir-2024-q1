d895b13b764bf4a0267bde7ce86d0f4a063f2a6b
ogabriel <gabrieloliver8991@gmail.com>
2024-03-08 20:02:10 -0300

    add transaction

---
+++ lib/rinha/bank/transaction.ex
@@ -0,0 +1,20 @@
+defmodule Rinha.Bank.Transaction do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "transactions" do
+    field :valor, :integer
+    field :tipo, :string
+    field :descriao, :string
+    field :client_id, :id
+
+    timestamps(type: :utc_datetime, updated_at: false)
+  end
+
+  @doc false
+  def changeset(transaction, attrs) do
+    transaction
+    |> cast(attrs, [:valor, :tipo, :descriao])
+    |> validate_required([:valor, :tipo, :descriao])
+  end
+end
---
+++ priv/repo/migrations/20240308224915_create_transactions.exs
@@ -0,0 +1,16 @@
+defmodule Rinha.Repo.Migrations.CreateTransactions do
+  use Ecto.Migration
+
+  def change do
+    create table(:transactions) do
+      add :valor, :integer
+      add :tipo, :string
+      add :descriao, :string
+      add :client_id, references(:clients, on_delete: :nothing)
+
+      timestamps(type: :utc_datetime, updated_at: false)
+    end
+
+    create index(:transactions, [:client_id])
+  end
+end
