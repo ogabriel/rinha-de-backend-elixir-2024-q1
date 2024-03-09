746cbd581ea2532eae8e9d4520211020890574ed
ogabriel <gabrieloliver8991@gmail.com>
2024-03-09 19:35:43 -0300

    refactor validations to inside bank and use atoms

--- lib/rinha/bank.ex
+++ lib/rinha/bank.ex
@@ -12,31 +12,47 @@ defmodule Rinha.Bank do
   def transacoes(id, transacao_params) do
     transacao = Transaction.parse(transacao_params, id)
 
-    credito_ou_debito =
-      if transacao.tipo == "c" do
-        transacao.valor
-      else
-        -1 * transacao.valor
-      end
+    if validate_transacao(transacao) do
+      credito_ou_debito =
+        if transacao.tipo == "c" do
+          transacao.valor
+        else
+          -1 * transacao.valor
+        end
 
-    query = from(c in Client, where: c.id == ^id, lock: "FOR UPDATE")
+      query = from(c in Client, where: c.id == ^id, lock: "FOR UPDATE")
 
-    Repo.transaction(fn repo ->
-      with [client] <- repo.all(query),
-           novo_saldo <- client.saldo + credito_ou_debito,
-           true <- novo_saldo >= -client.limite do
-        client
-        |> Ecto.Changeset.change(%{saldo: novo_saldo})
-        |> repo.update
+      Repo.transaction(fn repo ->
+        with [client] <- repo.all(query),
+             novo_saldo <- client.saldo + credito_ou_debito,
+             true <- novo_saldo >= -client.limite do
+          client
+          |> Ecto.Changeset.change(%{saldo: novo_saldo})
+          |> repo.update
 
-        repo.insert(transacao)
+          repo.insert(transacao)
 
-        %{saldo: novo_saldo, limite: client.limite}
-      else
-        [] -> repo.rollback(:not_found)
-        _ -> repo.rollback(nil)
-      end
-    end)
+          %{saldo: novo_saldo, limite: client.limite}
+        else
+          [] -> repo.rollback(:not_found)
+          _ -> repo.rollback(nil)
+        end
+      end)
+    end
+  end
+
+  @types ["c", "d"]
+
+  defp validate_transacao(transacao) do
+    if is_integer(transacao.valor) &&
+         transacao.valor > 0 &&
+         is_binary(transacao.tipo) &&
+         transacao.tipo in @types &&
+         is_binary(transacao.descricao) do
+      descricao_tamanho = String.length(transacao.descricao)
+
+      descricao_tamanho > 0 && descricao_tamanho < 11
+    end
   end
 
   def extrato(id) do
--- lib/rinha_web/controllers/client_controller.ex
+++ lib/rinha_web/controllers/client_controller.ex
@@ -7,37 +7,21 @@ defmodule RinhaWeb.ClientController do
     id = parse_id(id)
 
     if is_integer(id) && id > 0 && id < 6 do
-      if validate_params(params) do
-        case Bank.transacoes(id, params) do
-          {:ok, result} ->
-            conn
-            |> put_status(200)
-            |> json(result)
+      case Bank.transacoes(id, params) do
+        {:ok, result} ->
+          conn
+          |> put_status(200)
+          |> json(result)
 
-          {:error, :not_found} ->
-            send_resp(conn, 404, "")
+        {:error, :not_found} ->
+          send_resp(conn, 404, "")
 
-          _ ->
-            send_resp(conn, 422, "")
-        end
-      else
-        send_resp(conn, 422, "")
+        _ ->
+          send_resp(conn, 422, "")
       end
     else
       send_resp(conn, 404, "")
     end
-  end
-
-  defp validate_params(params) do
-    is_integer(params["valor"]) &&
-      params["valor"] > 0 &&
-      params["tipo"] in ["c", "d"] &&
-      is_binary(params["descricao"]) &&
-      validate_descricao(String.length(params["descricao"]))
-  end
-
-  defp validate_descricao(descricao_tamanho) do
-    descricao_tamanho > 0 && descricao_tamanho < 11
   end
 
   def extrato(conn, %{"id" => id}) do
