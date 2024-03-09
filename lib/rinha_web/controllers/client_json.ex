f5d62ea4b643eea76df4a122db013dd6f6a1abdf
ogabriel <gabrieloliver8991@gmail.com>
2024-03-09 08:27:28 -0300

    add extrato json

--- lib/rinha_web/controllers/client_json.ex
+++ lib/rinha_web/controllers/client_json.ex
@@ -1,2 +1,21 @@
 defmodule RinhaWeb.ClientJSON do
+  def extrato(%{client: cliente}) do
+    %{
+      saldo: %{
+        total: cliente.saldo,
+        data_extrato: DateTime.now!("Etc/UTC"),
+        limite: cliente.limite
+      },
+      ultimas_transacoes: for(transacao <- cliente.transactions, do: extrato_transacao(transacao))
+    }
+  end
+
+  defp extrato_transacao(transacao) do
+    %{
+      valor: transacao.valor,
+      tipo: transacao.tipo,
+      descricao: transacao.descricao,
+      realizada_em: transacao.inserted_at
+    }
+  end
 end
