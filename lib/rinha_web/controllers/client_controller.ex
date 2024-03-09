61df2002669f2ecb33d41b0f5c9ea4cf305348ae
ogabriel <gabrieloliver8991@gmail.com>
2024-03-09 08:29:29 -0300

    add extrato controller func

--- lib/rinha_web/controllers/client_controller.ex
+++ lib/rinha_web/controllers/client_controller.ex
@@ -9,4 +9,29 @@ defmodule RinhaWeb.ClientController do
   import Ecto.Query
 
   action_fallback RinhaWeb.FallbackController
+
+  def transacoes(conn, %{"id" => id} = params) do
+    send_resp(conn, 422, id)
+  end
+
+  def extrato(conn, %{"id" => id}) do
+    id = parse_id(id)
+
+    if is_number(id) && id > 0 do
+      with %Client{} = client <- Bank.extrato(id) do
+        render(conn, :extrato, client: client)
+      else
+        _ -> send_resp(conn, 404, "")
+      end
+    else
+      send_resp(conn, 404, "")
+    end
+  end
+
+  defp parse_id(id) do
+    case Integer.parse(id) do
+      {id, _} -> id
+      _ -> nil
+    end
+  end
 end
