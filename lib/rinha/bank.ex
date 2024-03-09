be1af65530fcd294d6687405bf6dd23ab5fe8a3e
ogabriel <gabrieloliver8991@gmail.com>
2024-03-09 15:09:52 -0300

    add logic to lock for update

--- lib/rinha/bank.ex
+++ lib/rinha/bank.ex
@@ -9,7 +9,34 @@ defmodule Rinha.Bank do
   alias Rinha.Bank.Client
   alias Rinha.Bank.Transaction
 
+  def transacoes(id, transacao_params) do
+    transacao = Transaction.parse(transacao_params, id)
 
+    credito_ou_debito =
+      if transacao.tipo == "c" do
+        transacao.valor
+      else
+        -1 * transacao.valor
+      end
+
+    query = from(c in Client, where: c.id == ^id, lock: "FOR UPDATE")
+
+    Repo.transaction(fn repo ->
+      with %Client{} = client <- repo.one(query),
+           novo_saldo <- client.saldo + credito_ou_debito,
+           true <- novo_saldo >= -client.limite do
+        client
+        |> Client.changeset(%{saldo: novo_saldo})
+        |> repo.update
+
+        repo.insert(transacao)
+
+        %{saldo: novo_saldo, limite: client.limite}
+      else
+        _ -> repo.rollback(:whatever)
+      end
+    end)
+  end
 
   def extrato(id) do
     transaction_query = from t in Transaction, order_by: [desc: t.id], limit: 10
